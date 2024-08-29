ddbaa9dfd8e14048c198e3311172f62b7138a283
andrejohnson-dev <aj.sfw.developer@gmail.com>
2024-08-29 15:17:59 -0400

    .

--- middleware/file-upload.js
+++ middleware/file-upload.js
@@ -1,7 +1,7 @@
 const multer = require('multer');
 const multerS3 = require('multer-s3');
 const { S3Client } = require('@aws-sdk/client-s3');
-
+const path = require('path');
 
 // Initialize the S3 client
 const s3 = new S3Client({
@@ -15,23 +15,49 @@ const s3 = new S3Client({
 
 
 // Set up Multer with S3 storage
+
 const upload = multer({
     storage: multerS3({
         s3: s3,
-        bucket: 'us-product-guide-images',
+        bucket: process.env.AWS_BUCKET_NAME,
         metadata: function (req, file, cb) {
             cb(null, { fieldName: file.fieldname });
         },
+        contentType: multerS3.AUTO_CONTENT_TYPE, // Automatically set content type
         key: function (_, file, cb) {
-            console.log('MULTER FILE', file)
+            console.log('MULTER FILE', file);
+
+            // Determine the folder based on the fieldname
             let folder = 'media/images/';
             if (file.fieldname === 'qrcode' || file.fieldname.includes('resourceQrCodeImage')) {
                 folder = 'media/qrcodes/';
             }
-            cb(null, folder + file.originalname)
 
+            // Extract the original file name without any folder path
+            const originalFileName = path.basename(file.originalname);
+
+            // Construct the full S3 object key (folder + file name)
+            cb(null, `${folder}${originalFileName}`);
         }
     })
 });
+
+// const upload = multer({
+//     storage: multerS3({
+//         s3: s3,
+//         bucket: process.env.AWS_SECRET_ACCESS_KEY,
+//         metadata: function (req, file, cb) {
+//             cb(null, { fieldName: file.fieldname });
+//         },
+//         key: function (_, file, cb) {
+//             console.log('MULTER FILE', file)
+//             let folder = 'media/images/';
+//             if (file.fieldname === 'qrcode' || file.fieldname.includes('resourceQrCodeImage')) {
+//                 folder = 'media/qrcodes/';
+//             }
+//             cb(null, folder + file.originalname)
+//         }
+//     })
+// });
 
 module.exports = upload;
